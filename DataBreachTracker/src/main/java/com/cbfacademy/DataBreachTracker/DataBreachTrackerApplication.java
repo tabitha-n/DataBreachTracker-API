818053c69d5f15eee683011d04dc771a07355337
tabitha-n <ncooro.t@gmail.com>
2025-08-19 12:35:17 +0100

    initial setup

---
+++ DataBreachTracker/src/main/java/com/cbfacademy/DataBreachTracker/DataBreachTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.cbfacademy.DataBreachTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBreachTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBreachTrackerApplication.class, args);
+	}
+
+}
---
+++ DataBreachTracker/src/test/java/com/cbfacademy/DataBreachTracker/DataBreachTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbfacademy.DataBreachTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBreachTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
