e082023fcbea71d4144a8f4f12e4e169d9089c73
tabitha-n <ncooro.t@gmail.com>
2025-09-01 16:14:44 +0100

    added comments to Breach.java

--- src/main/java/com/cbfacademy/DataBreachTracker/Breach.java
+++ src/main/java/com/cbfacademy/DataBreachTracker/Breach.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+// JPA Entity representing a data breach
 @Entity
 public class Breach {
 
