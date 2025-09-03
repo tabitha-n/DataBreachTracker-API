924a0a78543bb55d457c88e263361a33afdf2d74
tabitha-n <ncooro.t@gmail.com>
2025-09-03 09:54:14 +0100

    Separated the tests for the getters and setters

--- src/test/java/com/cbfacademy/DataBreachTracker/BreachTest.java
+++ src/test/java/com/cbfacademy/DataBreachTracker/BreachTest.java
@@ -5,21 +5,40 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
 public class BreachTest {
-
-    @Test
-    public void testBreachGettersAndSetters() {
+    
+    @Test 
+    public void testOrganisationGetterSetter() {
         Breach breach = new Breach();
         breach.setOrganisation("TestOrg");
-        breach.setDate(LocalDate.of(2023, 1, 1));
-        breach.setIncidentType("Hacking");
-        breach.setDataCompromised("Emails");
-        breach.setAffectedUsers(1000);
-
-        assertEquals("TestOrg", breach.getOrganisation());
-        assertEquals(LocalDate.of(2023, 1, 1), breach.getDate());
-        assertEquals("Hacking", breach.getIncidentType());
-        assertEquals("Emails", breach.getDataCompromised());
-        assertEquals(1000, breach.getAffectedUsers());  
+        assertEquals("TestOrg", breach.getOrganisation());  
     }
     
+    @Test
+    public void testDateGetterSetter() {
+        Breach breach = new Breach();
+        LocalDate date = LocalDate.of(2023, 10, 1);
+        breach.setDate(date);
+        assertEquals(date, breach.getDate());
+    }
+
+    @Test
+    public void testIncidentTypeGetterSetter() {
+        Breach breach = new Breach();
+        breach.setIncidentType("Hacking");
+        assertEquals("Hacking", breach.getIncidentType());  
+    }
+
+    @Test
+    public void testDataCompromisedGetterSetter() {
+        Breach breach = new Breach();
+        breach.setDataCompromised("Emails");
+        assertEquals("Emails", breach.getDataCompromised());
+    }
+
+    @Test
+    public void testAffectedUsersGetterSetter() {
+        Breach breach = new Breach();
+        breach.setAffectedUsers(1000);
+        assertEquals(1000, breach.getAffectedUsers());  
+    }
 }
